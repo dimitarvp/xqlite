5c226d6bde9dc932b0cf31d82728de2ed1bdca61
Dimitar Panayotov <mitko.p@gmail.com>
2019-12-17 00:23:04 +0200

    Add opening and closing of an sqlite db

--- lib/xqlite.ex
+++ lib/xqlite.ex
@@ -6,7 +6,10 @@ defmodule Xqlite do
   TODO: Add something more useful than a summary.
   """
 
+  alias Xqlite.Config
+
   @type conn :: {:connection, reference(), reference()}
+  @type open_result :: conn | {:error, term()}
 
   defguard is_conn(x)
            when is_tuple(x) and elem(x, 0) == :connection and is_reference(elem(x, 1)) and
@@ -15,4 +18,26 @@ defmodule Xqlite do
   @spec int2bool(0 | 1) :: true | false
   def int2bool(0), do: false
   def int2bool(1), do: true
+
+  @spec open(Config.db_name(), keyword()) :: open_result()
+  @doc """
+  Opens a handle to an sqlite3 database.
+  """
+  def open(path, cfg \\ Config.default())
+
+  def open(path, cfg) when is_binary(path), do: String.to_charlist(path) |> open(cfg)
+
+  def open(path, cfg) when is_list(path) do
+    :esqlite3.open(path, Config.get_exec_timeout(cfg))
+  end
+
+  @spec close(conn(), keyword()) :: :ok
+  @doc """
+  Closes the handle to the sqlite3 database.
+  """
+  def close(db, cfg \\ Config.default())
+
+  def close(db, cfg) do
+    :esqlite3.close(db, Config.get_exec_timeout(cfg))
+  end
 end
--- test/xqlite_test.exs
+++ test/xqlite_test.exs
@@ -1,4 +1,9 @@
 defmodule XqliteTest do
   use ExUnit.Case
   doctest Xqlite
+
+  test "open and close" do
+    {:ok, db} = Xqlite.open(":memory:")
+    :ok = Xqlite.close(db)
+  end
 end
