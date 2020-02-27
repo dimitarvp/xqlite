987f11e4ba9134239d1fe1a2cfc55f49090a72be
Dimitar Panayotov <mitko.p@gmail.com>
2020-02-27 18:33:32 +0200

    WARNING: Broken Rust compilation!
    This commit paves the way to having a working `open` + `close` scenario.
    It's currently not compiling and is posted so I can get help from other
    people.

--- lib/xqlite/rusqlite_nif.ex
+++ lib/xqlite/rusqlite_nif.ex
@@ -2,6 +2,8 @@ defmodule Xqlite.RusqliteNif do
   use Rustler, otp_app: :xqlite, crate: :xqlite_rusqlitenif
 
   def add(_a, _b), do: err()
+  def open(_db_name, _opts), do: err()
+  def close(_db_name), do: err()
 
   defp err, do: :erlang.nif_error(:nif_not_loaded)
 end
