bf59b29f050b8ffb16166a1553ca7f5308e210c6
Dimitar Panayotov <mitko.p@gmail.com>
2020-03-17 02:57:12 +0200

    Add `exec` function (still does not compile)
    and change the way reporting an error of already closed connection
    works.

--- lib/xqlite/rusqlite_nif.ex
+++ lib/xqlite/rusqlite_nif.ex
@@ -2,7 +2,8 @@ defmodule Xqlite.RusqliteNif do
   use Rustler, otp_app: :xqlite, crate: :xqlite_rusqlitenif
 
   def open(_db_name, _opts), do: err()
-  def close(_db_name), do: err()
+  def close(_conn), do: err()
+  def exec(_conn, _sql), do: err()
 
   defp err, do: :erlang.nif_error(:nif_not_loaded)
 end
