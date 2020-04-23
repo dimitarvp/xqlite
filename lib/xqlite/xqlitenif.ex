e1224b07a848de45f08fabddd0a50c8807c54f70
Dimitar Panayotov <mitko.p@gmail.com>
2020-04-24 01:50:11 +0300

    Reimplement `close` with Rustler `0.22-rc` syntax
    and reintroduce the `Option<Connection>` containing value of the `Mutex`
    since we want to replace the connection value with `None` after it is
    closed.

--- lib/xqlite/xqlitenif.ex
+++ lib/xqlite/xqlitenif.ex
@@ -2,8 +2,8 @@ defmodule XqliteNIF do
   use Rustler, otp_app: :xqlite, crate: :xqlitenif
 
   def open(_db_name, _opts), do: err()
+  def close(_conn), do: err()
 
-  # def close(_conn), do: err()
   # def exec(_conn, _sql), do: err()
 
   defp err, do: :erlang.nif_error(:nif_not_loaded)
--- native/xqlitenif/src/lib.rs
+++ native/xqlitenif/src/lib.rs
@@ -4,9 +4,9 @@ use rustler::{atoms, Encoder, Env, Term};
 use std::path::Path;
 use std::sync::Mutex;
 
-atoms!(ok, error, already_closed);
+atoms!(ok, error, cannot_close, already_closed);
 
-struct XqliteConnection(Mutex<Connection>);
+struct XqliteConnection(Mutex<Option<Connection>>);
 
 enum OpenResult {
     Success(ResourceArc<XqliteConnection>),
@@ -18,6 +18,22 @@ impl<'a> Encoder for OpenResult {
         match self {
             OpenResult::Success(arc) => (ok(), arc).encode(env),
             OpenResult::Failure(msg) => (error(), msg).encode(env),
+        }
+    }
+}
+
+enum CloseResult {
+    Success,
+    AlreadyClosed,
+    Failure(String)
+}
+
+impl<'a> Encoder for CloseResult {
+    fn encode<'b>(&self, env: Env<'b>) -> Term<'b> {
+        match self {
+            CloseResult::Success => (ok()).encode(env),
+            CloseResult::AlreadyClosed => (error(), already_closed()).encode(env),
+            CloseResult::Failure(msg) => (error(), cannot_close(), msg).encode(env),
         }
     }
 }
@@ -37,7 +53,7 @@ fn open(db_name: String, _opts: Vec<Term>) -> OpenResult {
 
     match Connection::open_with_flags(path, flags) {
         Ok(conn) => {
-            let mutex = Mutex::new(conn);
+            let mutex = Mutex::new(Some(conn));
             let xconn = XqliteConnection(mutex);
             let wrapper = ResourceArc::new(xconn);
             OpenResult::Success(wrapper)
@@ -49,4 +65,25 @@ fn open(db_name: String, _opts: Vec<Term>) -> OpenResult {
     }
 }
 
-rustler::init!("Elixir.XqliteNIF", [open], load = on_load);
+#[rustler::nif(schedule = "DirtyIo")]
+fn close(arc: ResourceArc<XqliteConnection>) -> CloseResult {
+    let mut mconn = arc.0.lock().unwrap();
+    if !mconn.is_none() {
+        // Take out the connection so it becomes None in the resource
+        // as it is being closed early.
+        let conn: Connection = mconn.take().unwrap();
+        match conn.close() {
+            Ok(()) => CloseResult::Success,
+            Err((conn, err)) => {
+                // Closing failed, put the connection back in the original container.
+                *mconn = Some(conn);
+                let msg: String = format!("{:?}", err);
+                CloseResult::Failure(msg)
+            }
+        }
+    } else {
+        CloseResult::AlreadyClosed
+    }
+}
+
+rustler::init!("Elixir.XqliteNIF", [open, close], load = on_load);
