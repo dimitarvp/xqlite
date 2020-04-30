e4f1fd68607825be952ac87f0e731a2ef07b8fb8
Dimitar Panayotov <mitko.p@gmail.com>
2020-04-30 03:21:50 +0300

    Add specs

--- lib/xqlite/util.ex
+++ lib/xqlite/util.ex
@@ -1,21 +1,25 @@
 defmodule Xqlite.Util do
+  @spec readable_pragma_with_zero_args?({atom(), keyword()}) :: boolean()
   def readable_pragma_with_zero_args?({_name, kw}) when is_list(kw) do
     kw
     |> Keyword.get_values(:r)
     |> Enum.any?(fn x -> match?({0, _, _}, x) end)
   end
 
+  @spec readable_pragma_with_one_arg?({atom(), keyword()}) :: boolean()
   def readable_pragma_with_one_arg?({_name, kw}) when is_list(kw) do
     kw
     |> Keyword.get_values(:r)
     |> Enum.any?(fn x -> match?({1, _, _, _}, x) end)
   end
 
+  @spec writable_pragma_with_one_arg?({atom(), keyword()}) :: boolean
   def writable_pragma_with_one_arg?({_name, kw}) when is_list(kw) do
     kw
     |> Keyword.has_key?(:w)
   end
 
+  @spec pragmas_of_type(%{required(atom()) => keyword()}, atom()) :: [atom()]
   def pragmas_of_type(%{} = m, t) when is_atom(t) do
     m
     |> Stream.filter(fn {_name, kw} ->
@@ -31,6 +35,7 @@ defmodule Xqlite.Util do
     |> Enum.sort()
   end
 
+  @spec filter_pragmas(%{required(atom()) => keyword()}, atom()) :: [atom()]
   def filter_pragmas(%{} = m, filter) when is_function(filter, 1) do
     m
     |> Stream.filter(fn {name, kw} -> filter.({name, kw}) end)
