84a0bde1aa7a89dd874bef0cff92092a25c22ea9
Dimitar Panayotov <mitko.p@gmail.com>
2020-05-01 16:12:02 +0300

    Add pragma put tests, remove NIF pragma read tests

--- test/pragma_test.exs
+++ test/pragma_test.exs
@@ -1,17 +1,17 @@
 defmodule XqlitePragmaTest do
-  use ExUnit.Case
+  use ExUnit.Case, async: true
   doctest Xqlite.Pragma
 
   alias Xqlite.Conn
   alias Xqlite.Pragma, as: P
-  alias XqliteNIF, as: NIF
 
-  setup_all do
+  setup do
     {:ok, db} = Conn.open(Xqlite.unnamed_memory_db())
     {:ok, db: db}
   end
 
-  describe "pragma getting through our wrapper" do
+  # All readable PRAGMAs with zero args.
+  describe "read pragma with no arguments" do
     P.readable_with_zero_args()
     |> Enum.each(fn name ->
       test name, %{db: db} do
@@ -20,11 +20,12 @@ defmodule XqlitePragmaTest do
     end)
   end
 
-  describe "pragma getting through the NIF" do
-    P.readable_with_zero_args()
-    |> Enum.each(fn name ->
+  # All writable PRAGMAs with one arg.
+  describe "write pragma with one argument" do
+    P.valid_writes()
+    |> Enum.each(fn {name, arg_spec} ->
       test name, %{db: db} do
-        assert valid_pragma(NIF.pragma_get0(db, Atom.to_string(unquote(name)), []))
+        assert valid_pragma_write(db, unquote(name), unquote(Macro.escape(arg_spec)))
       end
     end)
   end
@@ -32,4 +33,25 @@ defmodule XqlitePragmaTest do
   defp valid_pragma({:error, _, _}), do: false
   defp valid_pragma({:error, _}), do: false
   defp valid_pragma({:ok, _}), do: true
+
+  defp valid_put_result(_name, :ok), do: true
+  defp valid_put_result(_name, {:ok, _result}), do: true
+
+  defp valid_put_result(name, {:error, :unsupported_pragma_put_value, val}) do
+    flunk("PRAGMA put #{name} failed: unsupported value: #{val}")
+  end
+
+  defp valid_put_result(name, {:error, :already_closed}) do
+    flunk("PRAGMA put #{name} failed: connection has been closed")
+  end
+
+  defp valid_put_result(name, {:error, :pragma_put_failed, msg}) do
+    flunk("PRAGMA put #{name} failed with error message: #{msg}")
+  end
+
+  defp valid_pragma_write(db, name, enumerable) do
+    val = Enum.random(enumerable)
+    result = P.put(db, name, val)
+    valid_put_result(name, result)
+  end
 end
