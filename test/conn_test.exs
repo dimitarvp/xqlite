e162333dd0a2ec0c4a445ff80b85a1321b5e992e
Dimitar Panayotov <mitko.p@gmail.com>
2020-12-18 00:03:54 +0200

    Separate conn and NIF tests

--- test/conn_test.exs
+++ test/conn_test.exs
@@ -2,17 +2,10 @@ defmodule XqliteConnTest do
   use ExUnit.Case
 
   alias Xqlite.Conn
-  alias XqliteNIF, as: NIF
 
   test "opening and closing through our wrapper" do
     {:ok, db} = Conn.open(Xqlite.anon_db())
     :ok = Conn.close(db)
     {:error, :already_closed} = Conn.close(db)
-  end
-
-  test "opening and closing through the NIF" do
-    {:ok, db} = NIF.open(Xqlite.anon_db(), [])
-    :ok = NIF.close(db)
-    {:error, :already_closed} = NIF.close(db)
   end
 end
