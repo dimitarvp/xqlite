b17b7c6653b47f05904bffb58af68df327dceeac
Dimitar Panayotov <mitko.p@gmail.com>
2025-04-13 03:27:39 +0300

    avoid lifetime issues via stringifying errors and
    a big refactor to accomodate for it. Also utilize `From`, `?` and normal
    error handling.

--- native/xqlitenif/src/r2d2.rs
+++ native/xqlitenif/src/r2d2.rs
@@ -1,14 +1,16 @@
 use crate::atoms::{
-    cannot_allocate_binary, cannot_encode, cannot_execute, cannot_fetch_row,
-    cannot_open_database, cannot_prepare_statement, connection_not_found, timeout,
-    unsupported_value,
+    cannot_allocate_binary, cannot_convert_atom_to_string, cannot_decode, cannot_execute,
+    cannot_fetch_row, cannot_open_database, cannot_prepare_statement, cannot_read_column,
+    connection_not_found, r#false, r#true, timeout, unsupported_value,
 };
 use dashmap::DashMap;
 use r2d2::Pool;
 use r2d2_sqlite::SqliteConnectionManager;
-use rustler::resource_impl;
+use rusqlite::{types::Value, ToSql};
 use rustler::types::atom::{error, nil, ok};
-use rustler::{Encoder, Env, OwnedBinary, Resource, ResourceArc, Term};
+use rustler::{resource_impl, Atom, Binary, ListIterator, TermType};
+use rustler::{Encoder, Env, Error as RustlerError, OwnedBinary, Resource, ResourceArc, Term};
+use std::fmt::{self, Debug, Display};
 use std::panic::RefUnwindSafe;
 use std::sync::atomic::{AtomicU64, Ordering};
 use std::sync::OnceLock;
@@ -34,6 +36,7 @@ impl Encoder for XqliteConn {
 
 #[derive(Debug)]
 pub(crate) struct XqliteVal(rusqlite::types::Value);
+
 impl Encoder for XqliteVal {
     fn encode<'a>(&self, env: Env<'a>) -> Term<'a> {
         match &self.0 {
@@ -41,67 +44,209 @@ impl Encoder for XqliteVal {
             rusqlite::types::Value::Integer(i) => i.encode(env),
             rusqlite::types::Value::Real(f) => f.encode(env),
             rusqlite::types::Value::Text(s) => s.encode(env),
-            rusqlite::types::Value::Blob(b) => {
-                let mut owned_binary = match OwnedBinary::new(b.len()) {
-                    Some(owned_binary) => owned_binary,
-                    None => return XqliteError::CannotAllocateBinary.encode(env),
-                };
-
-                owned_binary.as_mut_slice().copy_from_slice(b);
-                let binary = owned_binary.release(env);
-                binary.encode(env)
-            }
+            rusqlite::types::Value::Blob(b) => match OwnedBinary::new(b.len()) {
+                Some(mut owned_binary) => {
+                    owned_binary.as_mut_slice().copy_from_slice(b);
+                    owned_binary.release(env).encode(env)
+                }
+                None => (error(), XqliteError::CannotAllocateBinary).encode(env),
+            },
         }
     }
 }
 
 #[derive(Debug)]
-enum XqliteError<'a> {
-    UnsupportedValue(Term<'a>),
+pub(crate) enum XqliteError {
+    UnsupportedValue { value_str: String },
+    CannotDecodeValue { value_str: String, reason: String },
     ConnectionNotFound(XqliteConn),
-    Timeout(r2d2::Error),
-    CannotPrepareStatement(String, rusqlite::Error),
-    CannotEncodeValue(usize, rusqlite::Error),
-    CannotExecute(rusqlite::Error),
-    CannotFetchRow(rusqlite::Error),
+    Timeout(String),
+    CannotPrepareStatement(String, String),
+    CannotReadColumn(usize, String),
+    CannotExecute(String),
+    CannotFetchRow(String),
     CannotAllocateBinary,
-    CannotOpenDatabase(String, r2d2::Error),
+    CannotOpenDatabase(String, String),
+    CannotConvertAtomToString(String),
 }
 
-impl Encoder for XqliteError<'_> {
-    fn encode<'a>(&self, env: Env<'a>) -> Term<'a> {
+impl Display for XqliteError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
-            XqliteError::UnsupportedValue(term) => (unsupported_value(), term).encode(env),
+            XqliteError::UnsupportedValue { value_str } => {
+                write!(f, "Unsupported value: {}", value_str)
+            }
+            XqliteError::CannotDecodeValue { value_str, reason } => {
+                write!(f, "Cannot decode value '{}': {}", value_str, reason)
+            }
             XqliteError::ConnectionNotFound(conn) => {
-                (connection_not_found(), conn).encode(env)
+                write!(f, "Connection not found: {:?}", conn)
             }
-            XqliteError::Timeout(err) => (timeout(), err.to_string()).encode(env),
-            XqliteError::CannotPrepareStatement(sql, err) => {
-                (cannot_prepare_statement(), sql, err.to_string()).encode(env)
+            XqliteError::Timeout(reason) => {
+                write!(f, "Timeout getting connection: {}", reason)
             }
-            XqliteError::CannotEncodeValue(index, err) => {
-                (cannot_encode(), index, err.to_string()).encode(env)
+            XqliteError::CannotPrepareStatement(sql, reason) => {
+                write!(f, "Cannot prepare statement '{}': {}", sql, reason)
             }
-            XqliteError::CannotExecute(err) => (cannot_execute(), err.to_string()).encode(env),
-            XqliteError::CannotFetchRow(err) => {
-                (cannot_fetch_row(), err.to_string()).encode(env)
+            XqliteError::CannotReadColumn(index, reason) => {
+                write!(f, "Cannot read column value at index {}: {}", index, reason)
             }
-            XqliteError::CannotAllocateBinary => (cannot_allocate_binary()).encode(env),
-            XqliteError::CannotOpenDatabase(path, err) => {
-                (cannot_open_database(), path, err.to_string()).encode(env)
+            XqliteError::CannotExecute(reason) => {
+                write!(f, "Cannot execute query: {}", reason)
+            }
+            XqliteError::CannotFetchRow(reason) => write!(f, "Cannot fetch row: {}", reason),
+            XqliteError::CannotAllocateBinary => write!(f, "Cannot allocate binary for blob"),
+            XqliteError::CannotOpenDatabase(path, reason) => {
+                write!(f, "Cannot open database '{}': {}", path, reason)
+            }
+            XqliteError::CannotConvertAtomToString(reason) => {
+                write!(f, "Cannot convert Elixir atom to string: {}", reason)
             }
         }
     }
 }
 
-impl RefUnwindSafe for XqliteError<'_> {}
+impl Encoder for XqliteError {
+    fn encode<'a>(&self, env: Env<'a>) -> Term<'a> {
+        match self {
+            XqliteError::UnsupportedValue { value_str } => {
+                (unsupported_value(), value_str).encode(env)
+            }
+            XqliteError::ConnectionNotFound(conn) => {
+                (connection_not_found(), conn).encode(env)
+            }
+            XqliteError::Timeout(reason) => (timeout(), reason).encode(env),
+            XqliteError::CannotPrepareStatement(sql, reason) => {
+                (cannot_prepare_statement(), sql, reason).encode(env)
+            }
+            XqliteError::CannotReadColumn(index, reason) => {
+                (cannot_read_column(), index, reason).encode(env)
+            }
+            XqliteError::CannotDecodeValue { value_str, reason } => {
+                (cannot_decode(), value_str, reason).encode(env)
+            }
+            XqliteError::CannotExecute(reason) => (cannot_execute(), reason).encode(env),
+            XqliteError::CannotFetchRow(reason) => (cannot_fetch_row(), reason).encode(env),
+            XqliteError::CannotAllocateBinary => cannot_allocate_binary().encode(env),
+            XqliteError::CannotOpenDatabase(path, reason) => {
+                (cannot_open_database(), path, reason).encode(env)
+            }
+            XqliteError::CannotConvertAtomToString(reason) => {
+                (cannot_convert_atom_to_string(), reason).encode(env)
+            }
+        }
+    }
+}
 
-// Initialize only once: global map of SQLite connection pools.
+impl RefUnwindSafe for XqliteError {}
+
+impl From<r2d2::Error> for XqliteError {
+    fn from(err: r2d2::Error) -> Self {
+        XqliteError::Timeout(err.to_string())
+    }
+}
+
+impl From<rusqlite::Error> for XqliteError {
+    fn from(err: rusqlite::Error) -> Self {
+        XqliteError::CannotExecute(err.to_string())
+    }
+}
+
+fn elixir_term_to_rusqlite_value<'a>(
+    env: Env<'a>,
+    term: Term<'a>,
+) -> Result<Value, XqliteError> {
+    let make_decode_error = |term: Term<'a>, err: RustlerError| -> XqliteError {
+        XqliteError::CannotDecodeValue {
+            value_str: format!("{:?}", term),
+            reason: format!("{:?}", err),
+        }
+    };
+    let make_unsupported_error = |term: Term<'a>| -> XqliteError {
+        XqliteError::UnsupportedValue {
+            value_str: format!("{:?}", term),
+        }
+    };
+
+    match term.get_type() {
+        TermType::Atom => {
+            if term == nil().to_term(env) {
+                Ok(Value::Null)
+            } else if term == r#true().to_term(env) {
+                Ok(Value::Integer(1))
+            } else if term == r#false().to_term(env) {
+                Ok(Value::Integer(0))
+            } else {
+                Err(make_unsupported_error(term))
+            }
+        }
+        TermType::Integer => term
+            .decode::<i64>()
+            .map(Value::Integer)
+            .map_err(|e| make_decode_error(term, e)),
+        TermType::Float => term
+            .decode::<f64>()
+            .map(Value::Real)
+            .map_err(|e| make_decode_error(term, e)),
+        TermType::Binary => match term.decode::<String>() {
+            Ok(s) => Ok(Value::Text(s)),
+            // NOTE: We are not interested in the details of what failed when decoding to a String
+            // as it's most likely that the binary is not a valid UTF-8 string.
+            // We want to fall back to an Erlang binary if decoding to String fails and as such
+            // we are discarding the String decoding error.
+            Err(_string_decode_err) => match term.decode::<Binary>() {
+                Ok(bin) => Ok(Value::Blob(bin.as_slice().to_vec())),
+                Err(binary_decode_err) => Err(make_decode_error(term, binary_decode_err)),
+            },
+        },
+        TermType::List
+        | TermType::Map
+        | TermType::Tuple
+        | TermType::Fun
+        | TermType::Pid
+        | TermType::Port
+        | TermType::Ref
+        | TermType::Unknown => Err(make_unsupported_error(term)),
+    }
+}
+
+fn decode_keyword_params<'a>(
+    env: Env<'a>,
+    list_term: Term<'a>,
+) -> Result<Vec<(String, Value)>, XqliteError> {
+    let make_decode_error =
+        |term: Term<'a>, err: RustlerError, context: &str| -> XqliteError {
+            XqliteError::CannotDecodeValue {
+                value_str: format!("{:?}", term),
+                reason: format!("{} error: {:?}", context, err),
+            }
+        };
+
+    let iter: ListIterator<'a> = list_term
+        .decode()
+        .map_err(|e| make_decode_error(list_term, e, "Expected keyword list"))?;
+
+    let mut params: Vec<(String, Value)> = Vec::new();
+
+    for term_item in iter {
+        let (key_atom, value_term): (Atom, Term<'a>) = term_item.decode().map_err(|e| {
+            make_decode_error(term_item, e, "Keyword element not {atom, value} tuple")
+        })?;
+
+        let mut key_string: String = key_atom
+            .to_term(env)
+            .atom_to_string()
+            .map_err(|e| XqliteError::CannotConvertAtomToString(format!("{:?}", e)))?;
+        key_string.insert(0, ':'); // Prepend ':' for SQLite named parameters
+
+        let rusqlite_value = elixir_term_to_rusqlite_value(env, value_term)?;
+
+        params.push((key_string, rusqlite_value));
+    }
+    Ok(params)
+}
+
 static POOLS: OnceLock<XqlitePools> = OnceLock::new();
-
-// Initialize only once: an atomic counter for a non-locking monotonically increasing value.
-// This will be the "handle" to an SQLite connection that we return to the Elixir code.
-// It's also the key in the global SQLite connection map.
 static NEXT_ID: AtomicU64 = AtomicU64::new(0);
 
 fn pools() -> &'static XqlitePools {
@@ -110,14 +255,13 @@ fn pools() -> &'static XqlitePools {
 
 fn create_pool(path: &str) -> Result<u64, r2d2::Error> {
     let manager = SqliteConnectionManager::file(path);
-    let pool = Pool::new(manager)?;
+    let pool = Pool::builder().build(manager)?;
     let id = NEXT_ID.fetch_add(1, Ordering::SeqCst);
     pools().insert(id, pool);
     Ok(id)
 }
 
 fn get_pool(id: u64) -> Option<XqlitePool> {
-    // This could theoretically deadlock but we never hold onto mutable references to the map.
     pools().get(&id).map(|x| x.value().clone())
 }
 
@@ -125,68 +269,92 @@ fn remove_pool(id: u64) -> bool {
     pools().remove(&id).is_some()
 }
 
-#[rustler::nif(schedule = "DirtyIo")]
-fn nif_open<'a>(path: String) -> Result<ResourceArc<XqliteConn>, XqliteError<'a>> {
-    create_pool(&path).map_or_else(
-        |e| Err(XqliteError::CannotOpenDatabase(path, e)),
-        |id| Ok(ResourceArc::new(XqliteConn(id))),
-    )
+fn xqlite_open(path: &str) -> Result<ResourceArc<XqliteConn>, XqliteError> {
+    let path_clone = path.to_string();
+    let pool_id = create_pool(path)
+        .map_err(|e| XqliteError::CannotOpenDatabase(path_clone, e.to_string()))?;
+
+    let conn_handle = XqliteConn(pool_id);
+    let resource = ResourceArc::new(conn_handle);
+    Ok(resource)
 }
 
-#[rustler::nif(schedule = "DirtyIo")]
-fn nif_exec<'a>(
-    handle: ResourceArc<XqliteConn>,
-    sql: String,
-) -> Result<Vec<Vec<Result<XqliteVal, XqliteError<'a>>>>, XqliteError<'a>> {
-    let pool = match get_pool(handle.0) {
-        Some(pool) => pool,
-        None => return Err(XqliteError::ConnectionNotFound(*handle)),
-    };
+fn xqlite_exec<'a>(
+    env: Env<'a>,
+    handle: &XqliteConn,
+    sql: &str,
+    params_term: Term<'a>,
+) -> Result<Vec<Vec<XqliteVal>>, XqliteError> {
+    let pool = get_pool(handle.0).ok_or(XqliteError::ConnectionNotFound(*handle))?;
+    let conn = pool.get()?;
+    let named_params_vec = decode_keyword_params(env, params_term)?;
 
-    let conn = match pool.get() {
-        Ok(conn) => conn,
-        Err(e) => return Err(XqliteError::Timeout(e)),
-    };
-
-    let mut stmt = match conn.prepare(&sql) {
-        Ok(stmt) => stmt,
-        Err(e) => return Err(XqliteError::CannotPrepareStatement(sql, e)),
-    };
+    let sql_string = sql.to_string();
+    let mut stmt = conn
+        .prepare(sql)
+        .map_err(|e| XqliteError::CannotPrepareStatement(sql_string, e.to_string()))?;
 
     let column_count = stmt.column_count();
-    let mut results: Vec<Vec<Result<XqliteVal, XqliteError>>> = Vec::new();
 
-    let rows = match stmt.query_map([], |row| {
-        let mut row_values: Vec<Result<XqliteVal, XqliteError>> =
-            Vec::with_capacity(column_count);
+    let params_for_rusqlite: Vec<(&str, &dyn ToSql)> = named_params_vec
+        .iter()
+        .map(|(k, v)| (k.as_str(), v as &dyn ToSql))
+        .collect();
+
+    let mut rows = stmt.query(params_for_rusqlite.as_slice())?;
+
+    let mut results: Vec<Vec<XqliteVal>> = Vec::new();
+
+    while let Some(row) = rows
+        .next()
+        .map_err(|e| XqliteError::CannotFetchRow(e.to_string()))?
+    {
+        let mut row_values: Vec<XqliteVal> = Vec::with_capacity(column_count);
         for i in 0..column_count {
-            let value: Result<XqliteVal, XqliteError> = match row.get(i) {
-                Ok(val) => Ok(XqliteVal(val)),
-                Err(e) => Err(XqliteError::CannotEncodeValue(i, e)),
-            };
-            row_values.push(value);
+            let value = row
+                .get::<usize, Value>(i)
+                .map_err(|e| XqliteError::CannotReadColumn(i, e.to_string()))?;
+            row_values.push(XqliteVal(value));
         }
-        Ok(row_values)
-    }) {
-        Ok(mapped_rows) => mapped_rows,
-        Err(err) => return Err(XqliteError::CannotExecute(err)),
-    };
-
-    for row_result in rows {
-        match row_result {
-            Ok(row) => results.push(row),
-            Err(e) => return Err(XqliteError::CannotFetchRow(e)),
-        }
+        results.push(row_values);
     }
 
     Ok(results)
 }
 
-#[rustler::nif(schedule = "DirtyIo")]
-fn nif_close(env: Env, handle: ResourceArc<XqliteConn>) -> Term {
+fn xqlite_close(handle: &XqliteConn) -> Result<(), XqliteError> {
     if remove_pool(handle.0) {
-        ok().encode(env)
+        Ok(())
     } else {
-        (error(), (XqliteError::ConnectionNotFound(*handle))).encode(env)
+        Err(XqliteError::ConnectionNotFound(*handle))
+    }
+}
+
+#[rustler::nif(schedule = "DirtyIo")]
+fn raw_open(env: Env, path: String) -> Term {
+    match xqlite_open(&path) {
+        Ok(resource) => (ok(), resource).encode(env),
+        Err(err) => (error(), err).encode(env),
+    }
+}
+
+#[rustler::nif(schedule = "DirtyIo")]
+fn raw_exec<'a>(
+    env: Env<'a>,
+    handle: ResourceArc<XqliteConn>,
+    sql: String,
+    params_term: Term<'a>,
+) -> Term<'a> {
+    match xqlite_exec(env, &handle, &sql, params_term) {
+        Ok(results) => (ok(), results).encode(env),
+        Err(err) => (error(), err).encode(env),
+    }
+}
+
+#[rustler::nif(schedule = "DirtyIo")]
+fn raw_close(env: Env, handle: ResourceArc<XqliteConn>) -> Term {
+    match xqlite_close(&handle) {
+        Ok(()) => ok().encode(env),
+        Err(err) => (error(), err).encode(env),
     }
 }
