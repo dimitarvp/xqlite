5f83e46705551c2a71cf1d25456b06929118ffcf
Dimitar Panayotov <mitko.p@gmail.com>
2025-04-23 16:59:15 +0300

    renames

--- lib/xqlite/pragma_util.ex
+++ lib/xqlite/pragma_util.ex
@@ -1,21 +1,21 @@
 defmodule Xqlite.PragmaUtil do
   @moduledoc ~S"""
   A module with zero dependencies on the rest of the modules in this library.
-  Used to reduce boilerplate and slice and dice the pragmas collection (also used in tests).
+  Used to reduce boilerplate and slice and dice the pragma collection.
   """
 
   @type name :: atom()
   @type spec :: keyword()
   @type arg_type :: :blob | :bool | :int | :list | :nothing | :real | :text
   @type pragma :: {name(), spec()}
-  @type pragmas :: %{required(name()) => spec()}
+  @type pragma_specs :: %{required(name()) => spec()}
   @type filter :: (pragma() -> boolean())
 
   defguard is_name(x) when is_atom(x)
   defguard is_spec(x) when is_list(x)
   defguard is_arg_type(x) when x in [:blob, :bool, :int, :list, :nothing, :real, :text]
   defguard is_pragma(x) when is_tuple(x) and is_name(elem(x, 0)) and is_spec(elem(x, 1))
-  defguard is_pragmas(x) when is_map(x)
+  defguard is_pragma_specs(x) when is_map(x)
   defguard is_filter(x) when is_function(x, 1)
 
   @spec readable?(pragma()) :: boolean()
@@ -49,14 +49,14 @@ defmodule Xqlite.PragmaUtil do
     end)
   end
 
-  @spec of_type(pragmas(), arg_type()) :: [name()]
-  def of_type(pragmas, type) when is_pragmas(pragmas) and is_arg_type(type) do
-    filter(pragmas, fn pragma -> returns_type?(pragma, type) end)
+  @spec of_type(pragma_specs(), arg_type()) :: [name()]
+  def of_type(pragma_specs, type) when is_pragma_specs(pragma_specs) and is_arg_type(type) do
+    filter(pragma_specs, fn pragma -> returns_type?(pragma, type) end)
   end
 
-  @spec filter(pragmas(), filter()) :: [name()]
-  def filter(pragmas, func) when is_pragmas(pragmas) and is_filter(func) do
-    pragmas
+  @spec filter(pragma_specs(), filter()) :: [name()]
+  def filter(pragma_specs, func) when is_pragma_specs(pragma_specs) and is_filter(func) do
+    pragma_specs
     |> Stream.filter(fn pragma -> func.(pragma) end)
     |> Stream.map(fn {name, _spec} -> name end)
     |> Enum.sort()
