14224f3ffb9a53563755e82e09e6ef15eb7e8ed5
Dimitar Panayotov <mitko.p@gmail.com>
2025-05-10 02:32:13 +0300

    make dialyzer GitHub Actions friendly

--- mix.exs
+++ mix.exs
@@ -80,8 +80,18 @@ defmodule Xqlite.MixProject do
     ]
   end
 
-  defp dialyzer(_) do
-    []
+  defp dialyzer(_env) do
+    [
+      # Specifies the directory where core PLTs (OTP, Elixir stdlib) are stored.
+      plt_core_path: "priv/plts/",
+      # Specifies the path to the final project PLT file, which includes dependencies.
+      # Using {:no_warn, ...} suppresses warnings if the file doesn't exist initially.
+      plt_file: {:no_warn, "priv/plts/core.plt"}
+      # You might want to add other options here, for example:
+      # applications: [:credo, :another_dep], # To add specific apps to the PLT
+      # flags: ["-Wunmatched_returns", ...], # Custom Dialyzer flags
+      # For most projects, the defaults for 'applications' (includes project + deps) are fine.
+    ]
   end
 
   defp aliases do
